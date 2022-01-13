9c99f2d10ac5b6ec3ad4ccbe8fc106c5afecebd2
Joel Truher <joel@truher.org>
2022-01-13 12:57:20 -0800

    thinking about robot state machine

---
+++ simulator/frc/states.py
@@ -0,0 +1,9 @@
+import transitions
+
+states = ['chasing_cargo', 'chasing_bots', 'shooting', 'climbing']
+
+transitions = [
+    ['hunt_cargo', 'shooting', 'chasing_cargo'],
+    ['defending', ['chasing_cargo', 'shooting'], 'chasing_bots'],
+    ['endgame', '*', 'climbing']
+]
