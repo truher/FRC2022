a1ed02b0593da648c4fe0b24f72a43b5a4c9e943
Joel Truher <joel@truher.org>
2022-01-31 13:45:50 -0800

    simulate lots of shots on the upper goal
    to generate data to fit a model

--- simulator/tools/sweep.py
+++ simulator/tools/sweep.py
@@ -1,6 +1,5 @@
 import multiprocessing
-from typing import Any, List
-import matplotlib.pyplot as plt # type:ignore
+from typing import Any, List, Optional
 import numpy as np
 import pandas as pd # type:ignore
 import constants
@@ -8,114 +7,116 @@ import trajectory
 
 # simulate some ballistic paths to make a range-velocity-elevation guide
 
-SINGLE: bool = False
-#SINGLE: bool = True
 MUZZLE_VELOCITY_MIN_M_S: float = 5
-MUZZLE_VELOCITY_MAX_M_S: float = 20
+MUZZLE_VELOCITY_MAX_M_S: float = 14
 GUN_ELEVATION_MIN_DEGREES: float = 35
 GUN_ELEVATION_MAX_DEGREES: float = 85
 
-def target(target_range_m: float) -> Any:
-    return pd.DataFrame({
-        'x':[target_range_m - constants.TARGET_RADIUS_M,
-             target_range_m + constants.TARGET_RADIUS_M],
-        'y':[constants.TARGET_HEIGHT_M, constants.TARGET_HEIGHT_M]}
-    )
-
-def plot_results(bff: Any) -> None:
-    fig, ax1 = plt.subplots()
-    ax2=ax1.twinx()
-    ax1.scatter(bff['r'], bff['e'], label="energy J", color="red")
-    ax1.scatter(bff['r'], bff['v'], label="velocity m/s", color="blue")
-    ax2.scatter(bff['r'], bff['l'], label="elevation degrees", color="green")
-    ax1.legend()
-    ax2.legend()
-    plt.title('lowest energy for range')
-    fig.tight_layout()
-    plt.show()
-
-def run_single() -> List:
-    # single-thread for profiling
-    bf: List = []
-    range_range: List = list(np.arange(constants.TARGET_MIN_RANGE_M, constants.TARGET_MAX_RANGE_M, 2))
-    for r in range_range:
-        bf.append(sweep_gun(r))
-    return bf
-
-def run_multi() -> List:
+def run_multi() -> List[dict]:
     # pylint: disable=consider-using-with
     # multi-processing
-    range_range: List = list(np.arange(constants.TARGET_MIN_RANGE_M, constants.TARGET_MAX_RANGE_M, 1))
-    p: Any = multiprocessing.Pool(processes=6,
-                                  maxtasksperchild=100)
-    return p.imap_unordered(sweep_gun, range_range)
+    range_range: List = list(np.arange(constants.TARGET_MIN_RANGE_M, constants.TARGET_MAX_RANGE_M, 0.5))
+    p: Any = multiprocessing.Pool(processes=6, maxtasksperchild=100)
+    return p.imap_unordered(sweep_with_precision, range_range)
 
 def run_all() -> None:
-    bf: List
-    if SINGLE:
-        bf = run_single()
-    else:
-        bf = run_multi()
+    bf: List[dict] = []
+    multi_bf: List[List[dict]] = run_multi()
+    bf.extend([i for s in multi_bf for i in s])
+    print("the results")
+    print(bf)
     bff = pd.DataFrame(bf)
+    bff.to_csv('new_results.csv')
     print(bff.to_csv())
-    plot_results(bff)
 
-GUN_PRECISION = 0.01 # std dev of angle and velocity
-def sweep_gun(target_range_m) -> dict:
+def sweep_with_precision(target_range) -> List[dict]:
+    """
+        range_precision jitters the range estimate.
+            it doesn't matter that much, the target is pretty big.  the optimal shot gets a little
+            higher as precision gets worse.
+        gun_precision jitters both velocity and elevation.
+            it makes a huge difference.  at 1%, 90% close shots are possible and long shots are around
+            60%. at 5%, the best shot is 3m at 75% or so, and long shots decline to 25%.  at 10%
+            the best shot is 3m at 45%, long shots under 20%.  i'm sure this is just about the spread
+            being bigger than the target diameter.
+        overall hit rate is strongly dependent on range: under 4 meters, >90% is possible.  over
+            4m, 60% is possible.  i'm sure this is all about bouncing, so calibrating the bounce
+            model seems important.
+    """
+    bf: List[dict] = []
+    for range_precision in [0.02, 0.04, 0.06]:
+        for gun_precision in [0.01, 0.03, 0.05]:
+            for i in range(3): # a few points for training
+                print(f"try {i} sweep range {target_range} gun_precision {gun_precision} "
+                      f"range_precision {range_precision}")
+                one_sweep: Optional[dict] = sweep_gun(target_range, gun_precision, range_precision)
+                if one_sweep is not None:
+                    bf.append(one_sweep)
+    return bf
+
+def sweep_gun(target_range_m:float, gun_precision:float, range_precision:float) -> dict:
+    """
+    gun_precision: std dev of angle and velocity.  1% = best possible, 10% = unusable
+    range_precision std dev of range measurement.  2% = best possible, 10% = unusable
+    """
     min_energy_J: float = 1000
     best_v: float = 0
     best_el: float = 0
     best_p_hit = 0
+    skip: int = 0 # step faster in zero-hit regions
     for muzzle_velocity_m_s in np.arange(
-        MUZZLE_VELOCITY_MIN_M_S, MUZZLE_VELOCITY_MAX_M_S, 1):
-        print(f"range {target_range_m} velocity {muzzle_velocity_m_s}")
+        MUZZLE_VELOCITY_MIN_M_S, MUZZLE_VELOCITY_MAX_M_S, 0.5):
+        #print(f"range {target_range_m} velocity {muzzle_velocity_m_s}")
         for gun_elevation_degrees in np.arange(GUN_ELEVATION_MIN_DEGREES,
             GUN_ELEVATION_MAX_DEGREES, 1):
-            print(f"range {target_range_m} "
-                  f"velocity {muzzle_velocity_m_s} "
-                  f"elevation {gun_elevation_degrees}")
+            if skip > 0:
+                skip -= 1
+                continue
+            #print(f"range {target_range_m} "
+            #      f"velocity {muzzle_velocity_m_s} "
+            #      f"elevation {gun_elevation_degrees}")
             tries = 100
             hits = 0
-            all_tries = [] # the trajectories
             for i in range(tries):
-                actual_muzzle_velocity_m_s = muzzle_velocity_m_s * np.random.normal(1.0, GUN_PRECISION)
-                actual_gun_elevation_degrees = gun_elevation_degrees * np.random.normal(1.0, GUN_PRECISION)
-                outcome, energy_J, df = trajectory.run( # df is list of dicts
-                    target_range_m,
+                actual_muzzle_velocity_m_s = muzzle_velocity_m_s * np.random.normal(1.0, gun_precision)
+                actual_gun_elevation_degrees = gun_elevation_degrees * np.random.normal(1.0, gun_precision)
+                actual_target_range_m = target_range_m * np.random.normal(1.0, range_precision)
+                outcome, energy_J, _ = trajectory.run(
+                    actual_target_range_m,
                     actual_muzzle_velocity_m_s,
                     actual_gun_elevation_degrees,
-                    True # don't return each trajectory
+                    False # don't return each trajectory
                 )
-                all_tries += df
                 if outcome == "hit":
                     hits += 1
-            #print(f"hits {hits}")
             if hits == 0:
-                #print("no hits")
+                print("skip zero")
+                skip = 5
                 continue
             p_hit = hits/tries
-            summary: str = (
-                f"range {target_range_m} "
-                f"velocity {muzzle_velocity_m_s} "
-                f"elevation {gun_elevation_degrees} "
-                f"arrival energy {energy_J:.2f} "
-                f"p(hit) {p_hit}")
-            print(summary)
+            print(f"range {target_range_m} velocity {muzzle_velocity_m_s} "
+                  f"elevation {gun_elevation_degrees} arrival energy {energy_J:.2f} "
+                  f"p(hit) {p_hit}")
             if p_hit > best_p_hit:
                 print("new best")
                 min_energy_J = energy_J
                 best_v = muzzle_velocity_m_s
                 best_el = gun_elevation_degrees
                 best_p_hit = p_hit
-            if SINGLE:
-                af = pd.DataFrame(all_tries)
-                df2 = target(target_range_m)
-                plt.scatter(af['x'],af['y'], label="trajectories", s=2)
-                plt.plot(df2['x'],df2['y'], label="target")
-                plt.title(summary)
-                plt.show()
-## FIXME
-    return {'r': target_range_m, 'e': best_p_hit, 'v': best_v, 'l': best_el}
+            if best_p_hit - p_hit > 0.2: # go faster on the way down
+                print("skip down")
+                skip = 5
+    if min_energy_J == 1000:
+        return None
+    return {
+        'r': target_range_m,
+        'gp': gun_precision,
+        'rp': range_precision,
+        'e': min_energy_J,
+        'h': best_p_hit,
+        'v': best_v,
+        'l': best_el
+    }
 
 if __name__ == '__main__':
     # main is required to avoid mp deadlock
